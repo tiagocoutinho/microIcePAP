1c794863f29d00d427a6cce9872b65c776f4b56e
Jose Tiago Coutinho Macara <coutinho@esrf.fr>
2015-11-01 14:57:13 +0100

    Version 2

--- microIcePAP/icepap.py
+++ microIcePAP/icepap.py
@@ -1,9 +1,18 @@
 import inspect
 import weakref
 import functools
+import itertools
+import collections
+
+try:
+  from collections import OrderedDict
+except ImportError:
+  from ordereddict import OrderedDict
 
 from tcp import Socket
 
+class IcePAPException(Exception):
+  pass
 
 class Attr(object):
 
@@ -36,16 +45,44 @@ class Attr(object):
     return self._dtype(r.split()[1])
 
 
+MAX_AXIS = 128
+
+def iter_axis(start=None, stop=None, step=None, device=None):
+  if start is None: start = 1
+  if stop is None: stop = MAX_AXIS+1
+  if step is None: step = 1
+  for i in xrange(start, stop, step):
+    if i % 10 > 8:
+      continue
+    yield i
+
+_non_seq = str, unicode, buffer, bytes, bytearray
+
 class AxisAttr(Attr):
 
   class attr:
     def __init__(self, device, parent):
-      self.__device = weakref.ref(device)
-      self.__parent = weakref.ref(parent)
+      self.__device = device
+      self.__parent = parent
+
     def __getitem__(self, item):
-      return self.__parent().get(self.__device(), axis=item)
+      if isinstance(item, slice):
+        axes = iter_axis(start=item.start, stop=item.stop, step=item.step,
+                         device=self.__device)
+        self.__parent.get(self.__device, axes=axes)
+      else:
+        axes = item,
+        return self.__parent.get(self.__device, axes=axes)[item]
+
     def __setitem__(self, item, value):
-      return self.__parent().set(self.__device(), value, axis=item)
+      return self.__parent.set(self.__device, value, axis=item)
+
+    def __call__(self, *args, **kwargs):
+      if kwargs: # a set operation
+        pass
+      else:
+        return self.__parent.get(self.__device, axes=args)        
+
 
   def __init__(self, *args, **kwargs):
     Attr.__init__(self, *args, **kwargs)
@@ -58,12 +95,19 @@ class AxisAttr(Attr):
     else:
       device.socket.write("{0} {1} {2}\n".format(self.name, axis, value))
 
-  def get(self, device, axis=None):
-    if axis is None:
+  def get(self, device, axes=None):
+    if axes is None:
       return self.attr(device, self)
-    else:
-      r = device.socket.write_readline("?{0} {1}\n".format(self.name, axis))
-      return self._dtype(r.split()[1])
+    axes = tuple(axes)
+    axes_str = " ".join(map(str, axes))
+    result = device.socket.write_readline("?{0} {1}\n".format(self.name, axes_str))
+    try:
+      result = map(self._dtype, result.split()[1:])
+    except Exception as exc:
+      msg = "Error getting {0!r}.{1}[{2}]: {3}".format(device, self.name, 
+                                                       axes_str, result)
+      raise IcePAPException(msg)
+    return OrderedDict(zip(axes, result))
 
 
 AttrRO = functools.partial(Attr, read_only=True)
@@ -72,11 +116,12 @@ AxisAttrInt = functools.partial(AxisAttr, dtype=int)
 
 class IcePAP(object):
 
-  help = AttrRO("HELP")
+  help = Attr("HELP", dtype=str, read_only=True)
 
   velocity = AxisAttr("VELOCITY")
   acctime = AxisAttr("ACCTIME")
-  position = AxisAttr("FPOS")
+  pos = AxisAttr("POS")
+  fpos = AxisAttr("FPOS")
 
   def __init__(self, host, port=5000):
     self.__s = Socket(host, port)
@@ -84,6 +129,10 @@ class IcePAP(object):
   @property
   def socket(self):
     return self.__s
+
+  def __repr__(self):
+    name, s = self.__class__.__name__, self.__s
+    return "{0}({1}:{2})".format(name, s._host, s._port)
 
 
 class Axis(object):
@@ -105,3 +154,9 @@ class Axis(object):
     return dir(self.device)
 
 
+def get_axis(icepap, axis, config):
+  axis = Axis(icepap, axis)
+  for i in range(7):
+    for k, v in config.items():
+      setattr(axis, k, v)
+  return axis
